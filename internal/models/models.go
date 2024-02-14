cdbb44e8572f6579c22cef75085289a0f7d7528b
ElnurKoke <akezanpprynbek@gmail.com>
2024-02-15 02:31:31 +0600

    google tester login work

---
+++ internal/models/models.go
@@ -0,0 +1,77 @@
+package models
+
+import (
+	"time"
+
+	"github.com/gofrs/uuid"
+)
+
+type User struct {
+	Id             int
+	Email          string
+	Username       string
+	Password       string
+	RepeatPassword string
+	ExpiresAt      time.Time
+	IsAuth         bool
+}
+
+type GoogleLoginUserData struct {
+	ID        uuid.UUID
+	Name      string
+	Email     string
+	Password  string
+	Role      string
+	Photo     string
+	Verified  bool
+	Provider  string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+type Post struct {
+	Id          int
+	Title       string
+	Description string
+	Image       string
+	Category    []string
+	Author      string
+	Likes       int
+	Dislikes    int
+	CreateAt    time.Time
+}
+
+type Message struct {
+	Id          int
+	PostId      int
+	CommentId   int
+	Author      string
+	ReactAuthor string
+	Message     string
+	Active      int
+	CreateAt    time.Time
+}
+
+type Comment struct {
+	Id         int
+	PostId     int
+	Creator    string
+	Text       string
+	Likes      int
+	Dislikes   int
+	IsAuth     bool
+	Created_at time.Time
+}
+
+type Like struct {
+	UserID       int
+	PostID       int
+	Islike       int
+	CommentID    int
+	CountLike    int
+	Countdislike int
+}
+
+type Category struct {
+	Name string
+}
---
+++ internal/storage/authGoogle.go
@@ -0,0 +1,10 @@
+package storage
+
+func (a *AuthStorage) CreateUserGoogle(email, username string) error {
+	query := `INSERT INTO user(email, username, password) VALUES ($1, $2, $3);`
+	_, err := a.db.Exec(query, email, username, "google")
+	if err != nil {
+		return err
+	}
+	return nil
+}
